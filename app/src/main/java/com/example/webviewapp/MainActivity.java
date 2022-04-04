0ba597196e22a7f16af735d7b0cfc59dc6333f8c
b21ricni <b21ricni@student.his.se>
2022-04-04 15:28:57 +0200

    Rename

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -20,10 +20,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void showExternalWebPage(){
         // TODO: Add your code for showing external web page here
+        myWebView.loadUrl("https://wwwlab.iit.his.se/b21ricni/");
     }
 
     public void showInternalWebPage(){
         // TODO: Add your code for showing internal web page here
+        myWebView.loadUrl("file:///android_asset/about.html");
     }
 
     @Override
@@ -36,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         myWebView =findViewById(R.id.my_webview);
         myWebView.getSettings().setJavaScriptEnabled(true);
         myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("file:///android_asset/about.html");
+
 
         /*
         * Rename your App. Tip: Values->Strings --done--
@@ -90,11 +92,13 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_external_web) {
             Log.d("==>","Will display external web page");
+            showExternalWebPage();
             return true;
         }
 
         if (id == R.id.action_internal_web) {
             Log.d("==>","Will display internal web page");
+            showInternalWebPage();
             return true;
         }
 
